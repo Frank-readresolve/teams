729776c143109470677894b4d3f379b4d87f7d59
Frank MARSHALL <frank@readresolve.io>
2022-04-11 17:06:01 +0200

    Added address and team

--- teams-db/data.dml.sql
+++ teams-db/data.dml.sql
@@ -6,13 +6,14 @@
  * \q
  */
 INSERT INTO towns (zip_code, name) VALUES
-	('93100', 'Montreuil');
+	('93100', 'Montreuil'),
+	('75000', 'Paris');
 
 INSERT INTO addresses (road, town_id) VALUES
 	('14 Rue de la Beaune', (SELECT t.id FROM towns t WHERE t.zip_code = '93100'));
 	
 INSERT INTO teams (name) VALUES
-	('Formateurs'), ('Pedagogique');
+	('Formateurs'), ('Pedagogique'), ('Apprenants');
 	
 INSERT INTO members (first_name, last_name, home_address_id) VALUES
 	('Jeremy', 'BOJKO', (SELECT t.id FROM addresses t WHERE t.road = '14 Rue de la Beaune')),
